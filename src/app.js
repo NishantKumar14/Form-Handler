8fb9a31a3a010c535ec485381173b3781a36f77e
Nishant Kumar <nk534031@gmail.com>
2025-03-20 20:42:32 +0530

    Fix bugs

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
+import ejsMate from "ejs-mate";
+import MongoDBStore from "connect-mongo";
 import Session from "express-session";
 
 const app = express();
 
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", "views");
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
@@ -14,7 +16,7 @@ app.use(express.static("public"));
 const secret = process.env.SECRET || "thisshouldbebettersecret!";
 
 const store = MongoDBStore.create({
-    mongoUrl: dbUrl,
+    mongoUrl: process.env.MONGODB_URI,
     touchAfter: 24 * 60 * 60,
     crypto: {
         secret,
@@ -40,3 +42,8 @@ const sessionConfig = {
 };
 
 app.use(Session(sessionConfig));
+
+
+// Routes
+
+export { app };
