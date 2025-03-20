2503596014f0adf5cd288fc7a29fc907bd64d512
Nishant Kumar <nk534031@gmail.com>
2025-03-20 20:04:19 +0530

    Add middlwares

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import Session from "express-session";
 
 const app = express();
 
@@ -9,3 +10,33 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+
+const secret = process.env.SECRET || "thisshouldbebettersecret!";
+
+const store = MongoDBStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret,
+    },
+});
+
+store.on("error", function (err) {
+    console.log("Session store error ", err);
+});
+
+const sessionConfig = {
+    store,
+    name: "sessionHell",
+    secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        // secure: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+};
+
+app.use(Session(sessionConfig));
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,38 @@
+import { User } from "../models/users.model";
+
+const requireLogin = (req, res, next) => {
+    try {
+        if (!req.session?.user_id) {
+            req.session.returnTo = req.originalUrl;
+            return res.redirect("/login");
+        }
+        next();
+    } catch (error) {
+        console.error("Error in requireLogin middleware:", error);
+        next(error);
+    }
+};
+
+const setLocal = async (req, res, next) => {
+    try {
+        res.locals = {
+            url: req.url,
+            currentUser: req.session.user_id || "",
+            currentUsername: "",
+        };
+
+        if (req.session.user_id) {
+            const user = await User.findById(req.session.user_id); // User -> Not define
+            if (user) {
+                res.locals.currentUsername = user.username;
+            }
+        }
+
+        next();
+    } catch (error) {
+        console.error("Error in setLocal middleware: ", error);
+        next(error);
+    }
+};
+
+export { requireLogin, setLocal };
