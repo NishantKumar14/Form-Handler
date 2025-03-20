6dd41d22de759352dd6abc5254177d6e9b124b85
Nishant Kumar <nk534031@gmail.com>
2025-03-20 15:01:25 +0530

    Add basic middleares

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
--- src/index.js
+++ src/index.js
@@ -5,4 +5,5 @@ dotenv.config({
     path: "./.env",
 });
 
-connectDB();
+connectDB()
+
