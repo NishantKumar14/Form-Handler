5bdd2561bde8e56655b25b1a9195ab8e234ab64a
Nishant Kumar <nk534031@gmail.com>
2025-03-20 11:47:50 +0530

    Basic setup and add user model

---
+++ src/models/users.model.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: [true, "Username is required."],
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true,
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required."],
+        },
+        fullName: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        phoneNumber: {
+            type: String,
+            required: true,
+        },
+        formId: {
+            type: [String],
+        },
+        isAdmin: {
+            type: Boolean,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+userSchema.statics.findAndValidate = async function (username, password) {
+    const foundUser = await this.findOne({
+        username: username,
+    });
+    if (!foundUser) {
+        return false;
+    }
+    const isValid = await bcrypt.compare(password, foundUser.password);
+    return isValid ? foundUser : false;
+};
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
+
+export const User = mongoose.model("User", userSchema);
